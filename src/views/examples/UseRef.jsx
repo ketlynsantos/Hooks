7c6a6cef8d146337446f8219a5afd5f127ba8089
Ketlyn Santos <ketlyncastro2004@gmail.com>
2022-05-06 13:54:10 -0300

    UseRef

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -2,15 +2,33 @@ import React, { useState, useRef, useEffect } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 
+
+//Mesclando os dois inputs(cocatenando letra por letra)
+const merge = function(s1, s2) {
+    return [...s1].map(function(e, i) {
+        return `${e}${s2[i] || ""}`
+    }).join("")
+} 
+
 const UseRef = (props) => {
 
     const [value1, setValue1] = useState("")
+    const [value2, setValue2] = useState("")
     //UseRef vai retornar a referencia de um objeto, e a partir disso eu consigo ter um estado
     const count = useRef(0)
+    const myInput1 =  useRef(null)
+    const myInput2 = useRef(null)
 
     useEffect(function() {
         count.current = count.current + 1
+        myInput2.current.focus()
     }, [value1])
+
+    useEffect(function() {
+        count.current = count.current + 1
+        myInput1.current.focus()
+    }, [value2])
+
 
     return (
         <div className="UseRef">
@@ -22,11 +40,19 @@ const UseRef = (props) => {
             <div className="center">
                 <div>
                     <span className="text">Valor: </span>
-                    <span className="text">{value1} [</span>
+                    <span className="text">{merge(value1, value2)} [</span>
                     <span className="text red">{count.current}</span>
                     <span className="text">]</span>
                 </div>
-                <input type="text" className="input" value={value1} onChange={e => setValue1(e.target.value)} />
+                {/* Com o ref é adicionado uma referência do input no current */}
+                <input type="text" className="input" value={value1} ref={myInput1}
+                    onChange={e => setValue1(e.target.value)} />
+            </div>
+
+            <SectionTitle title="Exercício 2"/>
+            <div className="center">
+                <input type="text" className="input" value={value2} ref={myInput2}
+                    onChange={e => setValue2(e.target.value)} />
             </div>
         </div>
     )
